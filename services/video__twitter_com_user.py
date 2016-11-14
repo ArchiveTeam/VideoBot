58d696253aa3471618f2f9981ab3415fa4a642ef
Arkiver2 <Arkiver@hotmail.com>
2016-11-14 22:04:26 +0100

    twitter_com_user: grab the /media timeline. Reduce wait between adding tweets to 5 seconds.

--- services/video__twitter_com_user.py
+++ services/video__twitter_com_user.py
@@ -22,13 +22,13 @@ service_name = 'Twitter account'
 service_commands = ['twitteraccount', 'twitter_account', 'twitter-account', 'twitteruser', 'twitter_user', 'twitter-user', 'ta', 'tu']
 url_regex = r'^https?://(?:www\.)?twitter\.com/[^/]+$'
 url_prefix = 'https://twitter.com/'
-url_suffix = ''
+url_suffix = '/media'
 url_user = lambda url: re.search(r'^https?://(?:www\.)?twitter\.com/([^/]+)', url).group(1)
 url_id = lambda url: re.sub('&quot;', '"', extract_info(r'data-user-id="([0-9]+)"', url)[0])
 item_title = lambda url: re.sub('&quot;', '"', extract_info(r'data-name="([^"]+)"', url)[0])
 
 def process(service_file_name, command, user):
-    url = check_create_url(command[1], url_prefix, url_suffix)
+    url = check_create_url(command[1] + '/media', url_prefix, url_suffix)
     videotitle = item_title(url)
     videoid = url_id(url)
 
@@ -37,20 +37,19 @@ def process(service_file_name, command, user):
     response = requests.get(url)
     for tweet in re.findall(r'data-permalink-path="([^"]+)"', response.text):
         functions.archive.main(['!a', 'https://twitter.com' + tweet], config.irc_nick)
-        time.sleep(20)
-
+        time.sleep(5)
     if 'data-min-position' in response.text:
         max_position = re.search(r'data-min-position="([^"]+)"', response.text).group(1)
         while 'data-min-position' in response.text or response_json['min_position']:
             response = requests.get('https://twitter.com/i/profiles/show/'
-                + url_user(url) + '/timeline/tweets?include_available_features=1&include_entities=1&max_position='
+                + url_user(url) + '/media_timeline?include_available_features=1&include_entities=1&max_position='
                 + max_position + '&reset_error_state=false')
             if response.status_code == 200:
                 response_json = json.loads(response.text)
                 max_position = response_json['min_position']
                 for tweet in re.findall(r'data-permalink-path="([^"]+)"', response_json['items_html']):
                     functions.archive.main(['!a', 'https://twitter.com' + tweet], config.irc_nick)
-                    time.sleep(20)
+                    time.sleep(5)
             else:
                 yield(failed_extraction(user, service_name, url_id(url), 'nextpage'))
                 break
